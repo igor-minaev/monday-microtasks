9a342fc84ecb1781e1f1a01d381938ad1097b40f
Igor Minayeu <igor.minayeu@gmail.com>
2023-05-20 12:44:41 +0300

    microtask 1

---
+++ src/site/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+type FooterPropsType = {
+    titleForFooter: string
+}
+
+export const Footer = (props: FooterPropsType) => {
+    return (
+        <div>{props.titleForFooter}</div>
+    );
+};
+
---
+++ src/site/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+type HeaderPropsType = {
+    titleForHeader: string
+}
+
+export const Header = (props: HeaderPropsType) => {
+    return (
+        <div>{props.titleForHeader}</div>
+    );
+};
+
